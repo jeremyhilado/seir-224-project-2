e14f8c13d9144ad4ce5a29ffc7e43413e1adbd7e
Jeremy Hilado <Jeremy@Jeremys-MacBook-Pro-2.local>
2020-03-27 13:52:58 -0700

    setup api

--- react-music-app/src/App.js
+++ react-music-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import About from './Components/About'
 import ArtistDetail from './Components/ArtistDetail'
@@ -8,10 +8,46 @@ import Nav from './Components/Nav'
 import SearchResults from './Components/SearchResults'
 
 function App() {
+
+  const[audiodbArtistData, setAudiodbArtistData] = useState({})
+  const[lastfmArtistData, setLastfmArtistData] = useState({})
+  const[lastfmSearchData, setLastfmSearchData] = useState({})
+  const[artist, setArtist] = useState('')
+
+  useEffect(() => {
+    console.log('App - useEffect')
+    const audiodbArtistAPI = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`
+    const lastfmArtistAPI = `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=7405f10b4750ad36052b877fa5b52ed2&format=json`
+    const lastfmSearchAPI = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artist}&api_key=7405f10b4750ad36052b877fa5b52ed2&format=json&limit=10`
+    const makeApiCall = async() => {
+      const res1 = await fetch(audiodbArtistAPI)
+      const res2 = await fetch(lastfmArtistAPI)
+      const res3 = await fetch(lastfmSearchAPI)
+      const json1 = await res1.json()
+      const json2 = await res2.json()
+      const json3 = await res3.json()
+      setAudiodbArtistData(json1)
+      setLastfmArtistData(json2)
+      setLastfmSearchData(json3)
+    }
+    makeApiCall()
+  }, [artist])
+
+  console.log('App - audiodbArtistData', audiodbArtistData)
+  console.log('App - lastfmArtistData', lastfmArtistData)
+  console.log('App - lastfmSearchData', lastfmSearchData)
+
+  const handleSubmit = async artistInput => {
+    setArtist(artistInput)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="nav">
       </header>
+      <main>
+        <Form handleSubmit={handleSubmit} />
+      </main>
     </div>
   );
 }
--- react-music-app/src/Components/Form.js
+++ react-music-app/src/Components/Form.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Form() {
+function Form(props) {
+
+    console.log('Form - props', props)
+
+    const[artist, setArtist] = useState('')
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        props.handleSubmit(artist)
+        setArtist('')
+    }
+
+    const handleChange = e => {
+        setArtist(e.target.value)
+    }
+
     return(
         <div>
-
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    placeholder='Enter Artist Name'
+                    value={artist}
+                    onChange={handleChange}
+                />
+                <button>Search</button>
+            </form>
         </div>
     )
 }
