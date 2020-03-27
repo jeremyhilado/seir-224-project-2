bc853eba1f5340326aab81430bd8dd9aa2e5b261
Jeremy Hilado <Jeremy@Jeremys-MacBook-Pro-2.local>
2020-03-27 12:25:49 -0700

    setup component files

--- react-music-app/src/App.js
+++ react-music-app/src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import './App.css';
+import About from './Components/About'
+import ArtistDetail from './Components/ArtistDetail'
+import Footer from './Components/Footer'
+import Form from './Components/Form'
+import Nav from './Components/Nav'
+import SearchResults from './Components/SearchResults'
 
 function App() {
   return (
---
+++ react-music-app/src/Components/ArtistDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ArtistDetail() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default ArtistDetail
